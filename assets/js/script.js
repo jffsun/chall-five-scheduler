fddba0bc93a4eefe65764c24ab54c16fac079880
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-07 22:30:41 -0700

    storing and retrieving local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
 // Displaying current date
 var dateDisplayEl = $('#date-display');
-
+var _9amDescription = $('#9am-description')
+var _9amStored;
 function displayDate() {
     var todayDate = moment().format("MMM Do YY");
     dateDisplayEl.text(todayDate);
 }
 
+
 function init () {
     displayDate ();
+    getTask ();
+    saveTask ();
+}
+
+function saveTask () { 
+    
+    $("#9am-btn").click(function() {
+    
+        // Save to local storage 
+        localStorage.setItem("9amSet", JSON.stringify(_9amDescription.val()));
+  
+    })
+}
+
+
+function getTask () {
+    var _9amGet = localStorage.getItem("9amSet");
+    console.log(_9amGet);
+    
 }
 
 init ();
